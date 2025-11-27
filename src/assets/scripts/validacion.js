b64e4a630a487c5c65b6d8efa0ef7647c317f58c
[Laura Cuñado García] <[lcunado@uoc.edu]>
2025-11-27 21:34:23 +0100

    Corrijo script

--- src/assets/scripts/validacion.js
+++ src/assets/scripts/validacion.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const form = document.getElementById("contactoForm");
+  const form = document.getElementById("contacto-form");
 
   form.addEventListener("submit", function (event) {
     event.preventDefault();
